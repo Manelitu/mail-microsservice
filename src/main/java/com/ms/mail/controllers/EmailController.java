df39f31a9f563ab9460401353139199634f1f081
Manelito <emanuelwork1@gmail.com>
2023-08-07 20:28:42 -0300

    "feat: email service injection"

--- src/main/java/com/ms/mail/controllers/EmailController.java
+++ src/main/java/com/ms/mail/controllers/EmailController.java
@@ -1,7 +1,13 @@
 package com.ms.mail.controllers;
 
+import com.ms.mail.services.EmailService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmailController {
+    final private EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
 }
