75b4d9b406da63280a5423507c82b284fede4e3b
Manelito <emanuelwork1@gmail.com>
2023-08-07 20:40:15 -0300

    "feat: create send email endpoint"

--- src/main/java/com/ms/mail/controllers/EmailController.java
+++ src/main/java/com/ms/mail/controllers/EmailController.java
@@ -1,6 +1,14 @@
 package com.ms.mail.controllers;
 
+import com.ms.mail.dtos.EmailDto;
+import com.ms.mail.models.EmailModel;
 import com.ms.mail.services.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +17,15 @@ public class EmailController {
 
     public EmailController(EmailService emailService) {
         this.emailService = emailService;
+    }
+
+    @PostMapping("sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendMail(emailModel);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(emailModel);
     }
 }
