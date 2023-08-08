1d86e1c0766cd3b8dec19878dbdbbed892f3640f
Manelito <emanuelwork1@gmail.com>
2023-08-07 21:17:53 -0300

    "feat: create sendMail method"

--- src/main/java/com/ms/mail/services/EmailService.java
+++ src/main/java/com/ms/mail/services/EmailService.java
@@ -1,18 +1,42 @@
 package com.ms.mail.services;
 
+import com.ms.mail.enums.StatusEmail;
 import com.ms.mail.models.EmailModel;
 import com.ms.mail.repositories.EmailRepository;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
     final private EmailRepository emailRepository;
-
-    public EmailService(EmailRepository emailRepository) {
+    final private JavaMailSender emailSender;
+//
+    public EmailService(EmailRepository emailRepository, JavaMailSender emailSender) {
         this.emailRepository = emailRepository;
+        this.emailSender = emailSender;
     }
 
-    public void sendMail(EmailModel emailModel) {
+    public EmailModel sendMail(EmailModel emailModel) {
+        emailModel.setSendDateTime(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
 
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            emailModel =  emailRepository.save(emailModel);
+        }
+
+        return emailModel;
     }
 }
