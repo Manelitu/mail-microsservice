89f0e3eeb7752ffa4d7538f7116ec384f6367c78
Manelito <emanuelwork1@gmail.com>
2023-08-09 20:13:47 -0300

    "feat: adding message converter"

--- src/main/java/com/ms/mail/configs/RabbitMQConfig.java
+++ src/main/java/com/ms/mail/configs/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.ms.mail.configs;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +14,10 @@ public class RabbitMQConfig {
     @Bean
     public Queue queue() {
         return new Queue(this.queue, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
