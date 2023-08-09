b99ccc9ffef1dcea277bda58cf613ee8196a6efa
Manelito <emanuelwork1@gmail.com>
2023-08-09 19:49:30 -0300

    "refactor: change id type"

--- src/main/java/com/ms/mail/repositories/EmailRepository.java
+++ src/main/java/com/ms/mail/repositories/EmailRepository.java
@@ -3,6 +3,8 @@ package com.ms.mail.repositories;
 import com.ms.mail.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 
 }
